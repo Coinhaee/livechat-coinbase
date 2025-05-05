5c57e631112bff322140e1d964aaefe0c23d1887
Coinhaee <horungebinance2@gmail.com>
2025-05-05 19:46:55 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+let adminSocket = null;
+
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id);
+
+  socket.on('register-admin', (password) => {
+    if (password === 'admin') {
+      adminSocket = socket;
+      console.log('Admin registered');
+    }
+  });
+
+  socket.on('user-message', (data) => {
+    if (adminSocket) {
+      adminSocket.emit('user-message', data);
+    }
+  });
+
+  socket.on('admin-reply', (data) => {
+    io.emit('admin-reply', data);
+  });
+
+  socket.on('disconnect', () => {
+    if (socket === adminSocket) {
+      adminSocket = null;
+    }
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Livechat server running on port 3001');
+});
